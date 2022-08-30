5288faf121aa4b5588a97a2ff0e39d8bd7fc4234
seanjungmsba <seanjeong1996@gmail.com>
2022-08-30 15:53:03 -0400

    modified class

--- src/main/java/com/example/restservice/models/Counter.java
+++ src/main/java/com/example/restservice/models/Counter.java
@@ -1,29 +1,25 @@
 package com.example.restservice.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
 
-@Entity
+@Table
 public class Counter {
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
+    @PrimaryKey
+    private String name;
+
     private Long count;
 
-    protected Counter() {}
-
-    public Counter(Long count) {
-        setCounter(count);
+    public Counter() {
     }
 
-    public Long getCounter() {
+    public Counter(Long count, String name) {
+        this.count = count;
+        this.name = name;
+    }
+
+    public Long incrementAndGet() {
+        this.count += 1;
         return count;
     }
-
-    public void setCounter(Long count) {
-        this.count = count;
-    }
-
 }
--- src/main/java/com/example/restservice/repositories/CounterRepository.java
+++ src/main/java/com/example/restservice/repositories/CounterRepository.java
@@ -1,19 +1,9 @@
 package com.example.restservice.repositories;
 
 import com.example.restservice.models.Counter;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CounterRepository extends CrudRepository<Counter, Long> {
-    @Query(
-            value = "SELECT id, count, cast(inet_server_addr() as text) FROM counter WHERE id = 1",
-            nativeQuery = true)
-    String getCounterAndTrace();
-
-    @Query(
-            value = "UPDATE counter SET count = ?1 WHERE id = 1 RETURNING id, count, cast(inet_server_addr() as text)",
-            nativeQuery = true)
-    String setCounterAndTrace(Long count);
+public interface CounterRepository extends CrudRepository<Counter, String> {
 }
